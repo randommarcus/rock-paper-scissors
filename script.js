52a5fa7ab83e336111d283de2e67a1caaa1540fc
randommarcus <105854450+randommarcus@users.noreply.github.com>
2024-10-25 16:12:19 +0530

    Wrote a code to compare the choices of human and computer

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ console.log("Hello World!!");
 
 let computerChoice = " ";
 let humanChoice = " ";
+let computerScore = 0;
+let humanScore = 0;
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -23,3 +25,14 @@ function getHumanChoice() {
 
 getHumanChoice();
 console.log(humanChoice);
+
+function playRound(computerChoice, humanChoice) {
+    if ((computerChoice == "rock" && humanChoice == "scissors") || (computerChoice == "scissors" && humanChoice == "paper") || (computerChoice == "paper" && humanChoice == "rock"))
+        console.log("Computer Won!!")
+        computerScore += 1;
+    else if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "scissors" && computerChoice == "paper") || (humanChoice == "paper" && computerChoice == "rock"))
+        console.log("You Won!!")
+        humanScore += 1;
+    else
+        console.log ("That's a tie!!")
+}
